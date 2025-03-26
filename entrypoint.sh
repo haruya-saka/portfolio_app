2aa62cf6c690857ffd227c038f44c42938dd2904
haruya-saka <haruharusaka@outlook.jp>
2025-03-26 09:39:22 +0000

    render.yml 作成

---
+++ entrypoint.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+set -e
+
+echo "[entrypoint.sh] Starting script"
+
+echo "[entrypoint.sh] Current directory"
+ls -la
+
+cd ./backend
+echo "[entrypoint.sh] Removing old server.pid"
+rm -f tmp/pids/server.pid
+
+echo "[entrypoint.sh] Installing gems"
+bundle install
+
+
+echo "[entrypoint.sh] Precompiling assets"
+bundle exec rake assets:precompile
+bundle exec rake assets:clean
+
+echo "[entrypoint.sh] Running db:migrate"
+bundle exec rake db:migrate
+
+echo "[entrypoint.sh] Executing main process: $@"
+exec "$@"
